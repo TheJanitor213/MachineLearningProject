9e32203b86c165f4cdbe1af12ef3334bf54d4cbb
thejanitor213 <brucebeck995@gmail.com>
2017-07-10 10:53:25 +0200

    Things

--- breastCancer.py
+++ breastCancer.py
@@ -7,10 +7,10 @@ with open("breastCancerData.csv", 'r') as f:
     for i, line in enumerate(reader):
         s = line['diagnosis']
         if(s=="B"):
-            training = "1 1:"+line['radius_mean'] + " 2:"+line['texture_mean'] + " 3:"+ line['perimeter_mean'] + " 4:"+ line['area_mean'] + " 5:"+line['smoothness_mean'] + " 6:"+ line['compactness_mean'] + " 7:"+ line['concavity_mean'] + " 8:"+line['concave points_mean'] + " 9:"+ line['symmetry_mean'] +" 10:"+ line['fractal_dimension_mean']
+            training = "1 1:"+line['texture_mean'] + " 2:"+ line['area_mean'] + " 3:"+line['smoothness_mean'] + " 4:"+line['concave points_mean']
             trainingList.append(training)
         else:
-            training = "-1 1:"+line['radius_mean'] + " 2:"+line['texture_mean'] + " 3:"+ line['perimeter_mean'] + " 4:"+ line['area_mean'] + " 5:"+line['smoothness_mean'] + " 6:"+ line['compactness_mean'] + " 7:"+ line['concavity_mean'] + " 8:"+line['concave points_mean'] + " 9:"+ line['symmetry_mean'] +" 10:"+ line['fractal_dimension_mean']
+            training = "-1 1:"+line['texture_mean'] + " 2:"+ line['area_mean'] + " 3:"+line['smoothness_mean'] + " 4:"+line['concave points_mean']
             trainingList.append(training)
 for i in trainingList:
     print(i)
